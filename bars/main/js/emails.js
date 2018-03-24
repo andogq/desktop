ab401d7035d34b4932dc91603388e3345b5b6c82
Tom Anderson <portlester.tom@gmail.com>
2018-03-24 14:21:10 +1100

    Fetches unread emails, the sender and subject

--- bars/main/js/emails.js
+++ bars/main/js/emails.js
@@ -11,20 +11,27 @@ const fs = require("fs");
 const configFile = JSON.parse(fs.readFileSync("./config.json", {encoding: "utf8"}));
 // True or false depending on whether there is a connection or not
 let connected = false;
+// Holds the previous variables, so they can be checked that they're different as to not slow down DOM
+let previous = {};
+// Has all the IDs of the emails already in the preview
+let previewedEmailIds = [];
 
 /*************
 *   Global DOM
 *************/
 const emailConnectionDot = document.getElementById("emailConnectionDot");
 const unreadEmailCount = document.getElementById("unreadEmailCount");
+const emailPreviews = document.getElementById("emailPreviews");
 
 /****************
 *   DOM Functions
 ****************/
 function updateConnectionState() {
-    let states = connected == false ? ["disconnected", "connected"] : ["connected", "disconnected"];
-    emailConnectionDot.classList.add(states[0]);
-    emailConnectionDot.classList.remove(states[1]);
+    if (connected != previous.connected) {
+        let states = connected == false ? ["disconnected", "connected"] : ["connected", "disconnected"];
+        emailConnectionDot.classList.add(states[0]);
+        emailConnectionDot.classList.remove(states[1]);
+    }
 }
 
 function updateUnreadEmailCount(unreadEmails) {
@@ -34,19 +41,61 @@ function updateUnreadEmailCount(unreadEmails) {
     unreadEmailCount.innerHTML = unreadEmailString;
 }
 
+function addEmailPreview(emailId, sender, subject) {
+    if (previewedEmailIds.indexOf(emailId) == -1) {
+        let container = document.createElement("div");
+        container.classList.add("email");
+
+        let senderElement = document.createElement("h4");
+        senderElement.classList.add("emailSender");
+        senderElement.innerHTML = sender;
+        container.appendChild(senderElement);
+
+        let subjectElement = document.createElement("h5");
+        subjectElement.classList.add("emailSubject");
+        subjectElement.innerHTML = subject;
+        container.appendChild(subjectElement);
+
+        emailPreviews.appendChild(container);
+
+        previewedEmailIds.push(emailId);
+    }
+}
+
 /******************
 *   Email functions
 ******************/
 function checkUnread() {
     if (connected) {
-        account.search(["UNSEEN"], (err, results) => {
-            const unreadEmails = results.length;
+        account.search(["UNSEEN"], (err, emails) => {
+            const unreadEmails = emails.length;
             if (unreadEmails > 0) {
-                console.log(unreadEmails);
                 updateUnreadEmailCount(unreadEmails);
+                for (emailId of emails) {
+                    getEmail(emailId);
+                }
             }
         });
     }
+}
+
+function getEmail(emailId) {
+    const emailRequest = account.fetch(emailId, {bodies: "HEADER.FIELDS (FROM SUBJECT)"});
+    emailRequest.on("message", (msg) => {
+        msg.on("body", (stream) => {
+            let message = "";
+            stream.on("data", (chunk) => {
+                message += chunk.toString("utf8");
+            });
+            stream.once("end", () => {
+                [sender, subject] = message.split("\n");
+                sender = /From: (.+) </.exec(sender)[1];
+                subject = /Subject: (.+)/.exec(subject)[1];
+
+                addEmailPreview(emailId, sender, subject);
+            });
+        });
+    });
 }
 
 /*********************
@@ -67,22 +116,24 @@ let account = new imap(configFile.email);
 // Connection handler
 account.once("ready", () => {
     account.openBox("INBOX", true, (err, box) => {
-        console.log("Connected");
-        connected = true;
+        changeConnectionState("connected");
         // Ready to do functions
     });
 });
 
 // Connection end handler
 account.once('end', () => {
-  console.log('Connection ended');
-  connected = false;
+  changeConnectionState("disconnected");
 });
 
 // Error handler
 account.once('error', (err) => {
     console.log(err);
 });
+
+function changeConnectionState(newState) {
+    [connected, previous.connected] = newState == "connected" ? [true, false] : [false, true];
+}
 
 // Connect to the account
 account.connect();
