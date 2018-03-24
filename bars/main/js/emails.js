261cd643aea6b1ec16e914855e02aab05ee9db3b
Tom Anderson <portlester.tom@gmail.com>
2018-03-24 13:50:22 +1100

    Unread email counter

--- bars/main/js/emails.js
+++ bars/main/js/emails.js
@@ -1,27 +1,88 @@
+/**********
+*   Imports
+**********/
 const imap = require("imap");
 const fs = require("fs");
 
+/**********
+*   Globals
+**********/
+// Configuration file
 const configFile = JSON.parse(fs.readFileSync("./config.json", {encoding: "utf8"}));
+// True or false depending on whether there is a connection or not
+let connected = false;
 
-let schoolMail = new imap(configFile.email);
+/*************
+*   Global DOM
+*************/
+const emailConnectionDot = document.getElementById("emailConnectionDot");
+const unreadEmailCount = document.getElementById("unreadEmailCount");
 
-schoolMail.once("ready", () => {
-    console.log("Connected");
-    schoolMail.openBox("INBOX", true, (err, box) => {
-        if (err) console.log(err);
-        schoolMail.search(["UNSEEN"], (err, results) => {
-            console.log(`There are ${results.length} unread messages`)
-            schoolMail.end();
+/****************
+*   DOM Functions
+****************/
+function updateConnectionState() {
+    let states = connected == false ? ["disconnected", "connected"] : ["connected", "disconnected"];
+    emailConnectionDot.classList.add(states[0]);
+    emailConnectionDot.classList.remove(states[1]);
+}
+
+function updateUnreadEmailCount(unreadEmails) {
+    const unreadEmailString = ((unreadEmails == 0) ? "No unread emails" : (
+        (unreadEmails == 1) ? "1 unread email" : `${unreadEmails} unread emails`
+    ));
+    unreadEmailCount.innerHTML = unreadEmailString;
+}
+
+/******************
+*   Email functions
+******************/
+function checkUnread() {
+    if (connected) {
+        account.search(["UNSEEN"], (err, results) => {
+            const unreadEmails = results.length;
+            if (unreadEmails > 0) {
+                console.log(unreadEmails);
+                updateUnreadEmailCount(unreadEmails);
+            }
         });
+    }
+}
+
+/*********************
+*   Interval functions
+*********************/
+function intervalFunction() {
+    checkUnread();
+    updateConnectionState();
+}
+setInterval(intervalFunction, 1000);
+
+/*************
+*   IMAP Setup
+*************/
+// Make the account object
+let account = new imap(configFile.email);
+
+// Connection handler
+account.once("ready", () => {
+    account.openBox("INBOX", true, (err, box) => {
+        console.log("Connected");
+        connected = true;
+        // Ready to do functions
     });
 });
 
-schoolMail.once('error', (err) => {
-  console.log(err);
-});
-
-schoolMail.once('end', () => {
+// Connection end handler
+account.once('end', () => {
   console.log('Connection ended');
+  connected = false;
 });
 
-schoolMail.connect();
+// Error handler
+account.once('error', (err) => {
+    console.log(err);
+});
+
+// Connect to the account
+account.connect();
