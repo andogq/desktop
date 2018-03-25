bbb556ec41c0eb1ec07c8584701daa6f90745857
Tom Anderson <portlester.tom@gmail.com>
2018-03-26 10:26:19 +1100

    Cleaned up index file and condensed useless functions

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
+/**********
+*   Imports
+**********/
 const electron = require("electron");
 const url = require("url");
 const fs = require("fs");
 
-// Globals
-let screenHeight, screenWidth;
-const colors = JSON.parse(fs.readFileSync("colors.json", {encoding: "utf8"}));
+/***********
+*    Globals
+***********/
+// To be filled when electron ready
+let screenSize;
 
-// Returns the screen size
-function getScreenSize() {
-    const screenSize = electron.screen.getPrimaryDisplay().size;
-    return [screenSize.height, screenSize.width];
-}
-
-// Makes a bar
-function makeBar(x, y, height, width, content, directory) {
-    const percantageRegex = /(\d+)%/;
-
-    if (height.match(percantageRegex)) {
-        height = Math.round(screenHeight * (percantageRegex.exec(height)[1] / 100));
-    }
-    if (width.match(percantageRegex)) {
-        width = Math.round(screenWidth * (percantageRegex.exec(width)[1] / 100));
-    }
-    if (x.toString().match(percantageRegex)) {
-        x = Math.round(screenWidth * (percantageRegex.exec(x)[1] / 100));
-    }
-    if (y.toString().match(percantageRegex)) {
-        y = Math.round(screenHeight * (percantageRegex.exec(y)[1] / 100));
-    }
-
+// Makes the window and loads it with the file
+function makeWindow() {
     const windowOptions = {
-        width: width,
-        height: height,
-        x: x,
-        y: y,
+        width: screenSize.width,
+        height: screenSize.height,
+        x: 0,
+        y: 0,
+        // Keeps the window in the background and so the user can't change the size
         resizable: false,
         movable: false,
         minimizable: false,
@@ -42,38 +27,33 @@ function makeBar(x, y, height, width, content, directory) {
         focusable: false
     }
 
-    let win = new electron.BrowserWindow(windowOptions);
-    win.loadURL(`data:text/html;charset=UTF-8,${encodeURIComponent(content)}`, {baseURLForDataURL: `file://${__dirname}/bars/${directory}/`});
+    // Loads the color scheme and encodes it
+    let content = encodeURIComponent(replaceColors(`bars/main/index.html`));
 
-    return win;
+    // Makes the window and loads it
+    let win = new electron.BrowserWindow(windowOptions);
+    win.loadURL(`data:text/html;charset=UTF-8,${content}`, {baseURLForDataURL: `file://${__dirname}/bars/main/`});
 }
 
 // Finds the names of colors and replaces them with the theme color
 function replaceColors(file) {
+    const colors = JSON.parse(fs.readFileSync("colors.json", {encoding: "utf8"}));
     const colorNames = Object.keys(colors);
     let contents = fs.readFileSync(file, {encoding: "utf8"});
 
+    // Iterate over all the color names and search with a regex
     for (color of colorNames) {
         let regex = new RegExp(`(: |")${color}(;|";)`, "g");
-        console.log(`${color}: ${contents.match(regex)}`);
         contents = contents.replace(regex, `$1${colors[color]}$2`);
     }
     return contents;
 }
 
-// Loads the bars with their contents
-function loadBar() {
-    fs.readdir("./bars", (err, files) => {
-        for (directory of files) {
-            let config = JSON.parse(fs.readFileSync(`./bars/${directory}/config.json`));
-            let content = replaceColors(`bars/${directory}/index.html`);
-            makeBar(config.position.x, config.position.y, config.dimensions.height, config.dimensions.width, content, directory);
-        }
-    });
-}
-
+// When electron is ready
 electron.app.on("ready", () => {
-    [screenHeight, screenWidth] = getScreenSize();
+    // Get the height and width
+    screenSize = electron.screen.getPrimaryDisplay().size;
 
-    loadBar();
+    // Starts the window loading
+    makeWindow();
 });
